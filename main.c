f52fd4ca9e89b4f5d9c15cb19ab4a68340e4aa83
Bogdan Irimie <irimie.bogdan@outlook.com>
2014-11-01 16:11:38 +0200

    Add comments, change variable name.

--- main.c
+++ main.c
@@ -7,17 +7,18 @@
 
 int main() {
     int  pid, status;
-    char input[255];
-    char** commandWithArguments;
-    char* programName;
-    int size = 0, i = 0;
+    char input[255];                /* inserted by the user */
+    char** commandWithArguments;    /* contains the words in the input */
+    char* programName;              /* the command preceded by it's path */
+    int numberOfWords = 0;           /* number of words in the input */
+    int i = 0;
 
     while (1) {
         printf("$>");
         fflush(stdout);
         fgets(input, sizeof(input), stdin);
-        size = 0;
-        parseString(input, &commandWithArguments, &size);
+        numberOfWords = 0;
+        parseString(input, &commandWithArguments, &numberOfWords);
 
         if (strcmp(commandWithArguments[0], "exit") == 0) {
             return 0;
@@ -39,7 +40,7 @@ int main() {
             free(programName);
 
             /* free memory for each word in the array */
-            for (i = 0; i < size; i++) {
+            for (i = 0; i < numberOfWords; i++) {
                 if (commandWithArguments[i] != NULL) {
                     free(commandWithArguments[i]);
                 }
@@ -49,7 +50,6 @@ int main() {
             if (commandWithArguments != NULL) {
                 free(commandWithArguments);
             }
-
         }
     }
 }
